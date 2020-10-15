37a6679b7a08033551aac80d8c5395fd5b30df82
ahmedashin999 <ahmedashin999@gmail.com>
2020-10-15 19:45:54 +0600

    Updates

---
+++ precache-manifest.a3b5073e9b28665c95d296c734e9fa66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56506098b7e318bb7b374a1c132d45ae",
+    "url": "/sportsquiz/index.html"
+  },
+  {
+    "revision": "fdc4a6b7f1a335726f9a",
+    "url": "/sportsquiz/static/css/main.2f208ca0.chunk.css"
+  },
+  {
+    "revision": "5bd21a013a9f195b786d",
+    "url": "/sportsquiz/static/js/2.a8785fa3.chunk.js"
+  },
+  {
+    "revision": "0e77ccc9007464ffd437757d6ef9c957",
+    "url": "/sportsquiz/static/js/2.a8785fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc4a6b7f1a335726f9a",
+    "url": "/sportsquiz/static/js/main.344785e6.chunk.js"
+  },
+  {
+    "revision": "e1c168ed3cc485614512",
+    "url": "/sportsquiz/static/js/runtime-main.6ba2cf6b.js"
+  },
+  {
+    "revision": "4f5c04a06fecfb1b1bc86554e7b83a19",
+    "url": "/sportsquiz/static/media/hero.4f5c04a0.jpg"
+  }
+]);
